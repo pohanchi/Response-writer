14b431d56aa4c5b28cbab7dbee12aa0619b18425
pohanchi <aapp1420@gmail.com>
2021-07-26 10:18:39 +0800

    remove yaml and short script for future past question

--- short_scripts/extract_doqa_feautre_future_eval.sh
+++
@@ -1,5 +0,0 @@
-#!/user/bin/shell
-python extract_feature_bert_doqa_future.py --config preprocessing_configs/bert/DoQA/future/future_eval/bert_dev_doqa_cooking_HAE.yaml --future
-python extract_feature_bert_doqa_future.py --config preprocessing_configs/bert/DoQA/future/future_eval/bert_test_doqa_cooking_HAE.yaml --future
-python extract_feature_bert_doqa_future.py --config preprocessing_configs/bert/DoQA/future/future_eval/bert_test_doqa_movies_HAE.yaml --future
-python extract_feature_bert_doqa_future.py --config preprocessing_configs/bert/DoQA/future/future_eval/bert_test_doqa_travel_HAE.yaml --future
--- short_scripts/extract_quac_feature_future.sh
+++
@@ -1,3 +0,0 @@
-#!/user/bin/shell
-python extract_feature_bert_quac_truncated_future.py --config preprocessing_configs/bert/QuAC/base_dev_truncated_HAE_future.yaml
-python extract_feature_bert_quac_truncated_future.py --config preprocessing_configs/bert/QuAC/base_train_truncated_HAE_future.yaml
